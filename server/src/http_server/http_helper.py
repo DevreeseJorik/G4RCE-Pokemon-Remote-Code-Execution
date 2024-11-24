a60c53da7fc3384b29a16623ff08a689b3c21fe3
Jorik Devreese <jorikdevreese@hotmail.be>
2024-11-24 15:19:37 +0100

    server: http_helper: remove dangling whitespaces
    Signed-off-by: Jorik Devreese <jorikdevreese@hotmail.be>

--- server/src/http_server/http_helper.py
+++ server/src/http_server/http_helper.py
@@ -8,11 +8,11 @@ class B64SCCrypto():
 
     def b64decode(self, data):
         return b64decode(data.replace('-', '+').replace('_', '/'))
-    
+
     def sce_decrypt(self, data):
         checksum = int.from_bytes(data[:4], byteorder='big') ^ 0x4a3b2c1d
         return self.decrypt_sce_data(data[4:244], checksum | (checksum << 16))[4:]
-    
+
     def decrypt_sce_data(self, encrypted_data, state):
         decrypted_data = bytearray()
         for byte in encrypted_data:
@@ -20,4 +20,3 @@ class B64SCCrypto():
             keybyte = (state >> 16) & 0xff
             decrypted_data.append((byte ^ keybyte) & 0xff)
         return decrypted_data
-        
