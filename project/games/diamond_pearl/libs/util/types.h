36f50971d4d4cec81973dd20b8940bce155c1c13
Jorik Devreese <jorikdevreese@hotmail.be>
2024-12-15 19:58:19 +0100

    types.h: add signed variants of types
    Signed-off-by: Jorik Devreese <jorikdevreese@hotmail.be>

--- project/games/diamond_pearl/libs/util/types.h
+++ project/games/diamond_pearl/libs/util/types.h
@@ -1,11 +1,16 @@
 #ifndef _TYPES_H
 #define _TYPES_H
 
-typedef int s32;
 typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned int u32;
 typedef unsigned long long u64;
+
+typedef signed char s8;
+typedef signed short s16;
+typedef signed int s32;
+typedef signed long long s64;
+
 typedef int BOOL;
 typedef u8 S_BOOL;
 
