1fbb60d47e06a0c50bbf2af3605eb7ff8967f4a8
Jorik Devreese <jorikdevreese@hotmail.be>
2024-12-26 10:38:18 +0100

    project: {diamond, platinum}: generate_ase_script: save 2 bytes in conversion to ACE
    Signed-off-by: Jorik Devreese <jorikdevreese@hotmail.be>

--- project/games/diamond_pearl/generate_ase_script/src/inject_overlay_payload.asm
+++ project/games/diamond_pearl/generate_ase_script/src/inject_overlay_payload.asm
@@ -11,7 +11,7 @@
 _start:
 push {r0-r2, lr}
 
-add r3, #0x11
+add r3, #0x13
 ldmia r3!, {r0, r1, r2, r4}
 str r2, [r0, #0x0]                  @ overwrite LoadOverlay return instruction
 mov r2, #0x20                       @ size of data to copy
@@ -19,6 +19,7 @@ add r0, r3, #0x0                    @ source buffer address
 blx r4                              @ execute memcp_uint8
 
 pop {r0-r4, pc}
+nop
 
 _data:
 .word 0x020d75d8                    @ return instruction of LoadOverlay
